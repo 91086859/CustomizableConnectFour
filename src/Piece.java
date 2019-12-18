0098cd028903738194c64bb17b1df9fc54c414e7
Kuhdeen Hike <32179176+91086859@users.noreply.github.com>
2019-12-18 07:02:52 -0800

    Create Piece.java

---
+++ src/Piece.java
@@ -0,0 +1,19 @@
+public class Piece
+{
+    private int piece;
+
+    public Piece(int value)
+    {
+        piece = value;
+    } // end constructor
+
+    public int getPieceValue()
+    {
+        return piece;
+    } // end int getPieceValue
+
+    public void setPlaced(int value)
+    {
+        piece = value;
+    } // end void setPlaced
+} // end class Piece
